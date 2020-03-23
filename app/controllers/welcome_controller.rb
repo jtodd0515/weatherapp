1fff0962c27cf4faa727885bb314fe5de45c6a68
James Todd <jtodd0515@gmail.com>
2020-03-23 17:31:35 -0400

    weather app project

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,29 @@
+class WelcomeController < ApplicationController
+  def test
+  response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip={94040},units=imperial,&appid=#{ENV["open_weather_map_key"]}")
+  @location = response ["name"]
+  @temp = response["main"]["temp"]
+  @cloudiness = response["clouds"]["all"]
+  @weather_icon = response["weather"][0]["icon"]
+  @weather_description = response["weather"][0]["description"]
+  @windiness = response["wind"]["speed"]
+  end
+
+  def index
+    if params[:zip] != nil && params[:zip] != ""
+      response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zip]},units=imperial,&appid=#{ENV["open_weather_map_key"]}")
+      @status = response["cod"]
+      if @status != '404'
+        @location = response["name"]
+        @temp = response["main"]["temp"]
+        @cloudiness = response["clouds"]["all"]
+        @weather_icon = response["weather"][0]["icon"]
+        @weather_description = response["weather"][0]["description"]
+        @windiness = response["wind"]["speed"]
+      else
+        @error = response["message"]
+      end
+    end
+
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'test', to: 'welcome#test'
+  get 'index', to: 'welcome#index'
+  post 'index', to: 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#test'
+end
