c5a6a65c07fcfe6e620a8edce8862dd26fdeb6c2
James Todd <jtodd0515@gmail.com>
2020-05-20 00:05:17 -0400

    geocoder added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,7 +11,7 @@ class WelcomeController < ApplicationController
 
   def index
     if params[:zip] != nil && params[:zip] != ""
-      response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=27834,us&units=imperial&appid=#{ENV["open_weather_map_key"]}")
+      response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zip]},us&units=imperial&appid=#{ENV["open_weather_map_key"]}")
       @status = response["cod"]
       if @status != '404'
         @location = response["name"]
@@ -20,6 +20,9 @@ class WelcomeController < ApplicationController
         @weather_icon = response["weather"][0]["icon"]
         @weather_description = response["weather"][0]["description"]
         @windiness = response["wind"]["speed"]
+        @lat = response["coord"]["lat"]
+        @long = response["coord"]["lon"]
+        @state = Geocoder.search([@lat, @long])
       else
         @error = response["message"]
       end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'index', to: 'welcome#index'
   post 'index', to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#test'
+  root 'welcome#index'
 end
